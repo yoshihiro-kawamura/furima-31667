ab683b45e1bf280ff2467f734177ba82260961e8
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-13 12:47:33 +0900

    カラムの修正

--- db/migrate/20201113023650_devise_create_users.rb
+++ db/migrate/20201113023650_devise_create_users.rb
@@ -11,8 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false 
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
-      t.data   :birthday,           null: false
-      
+      t.date   :birthday,           null: false
       
       ## Recoverable
       t.string   :reset_password_token
