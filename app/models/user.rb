33d7b796747d6dd4b38fbce1a24e9ee65c3af8b1
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-15 19:47:36 +0900

    Validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
   JAPANESE_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/
+  JAPANESE_KATAKANA_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
   with_options presence: true do
     validates :email,uniqueness: true, format: { with: User:: VALID_EMAIL_REGEX }
     validates :password, format: { with: User:: VALID_PASSWORD_REGEX }
@@ -16,7 +17,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :first_name, format: { with: User:: JAPANESE_REGEX }  
     validates :last_name, format: { with: User:: JAPANESE_REGEX }  
-    validates :last_name_kana, format: { with: User:: JAPANESE_REGEX }  
-    validates :first_name_kana, format: { with: User:: JAPANESE_REGEX }  
+    validates :last_name_kana, format: { with: User:: JAPANESE_KATAKANA_REGEX }  
+    validates :first_name_kana, format: { with: User:: JAPANESE_KATAKANA_REGEX }  
  end
 end
