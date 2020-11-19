a8df52d858ccca8bcece2db0c9fcc15bc553a79d
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-19 11:23:05 +0900

    修正内容

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+      @item.destroy
+      redirect_to root_path
+    else
+      render :show
   end
 
   private
