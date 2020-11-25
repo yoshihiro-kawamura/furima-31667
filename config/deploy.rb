1741c764183d51ef4d0295ddb9dd750a7b635c82
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-25 11:20:01 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock  "3.14.1"
 
 # Capistranoのログの表示に利用する
-set :application, 'furima-31667 '
+set :application, 'furima-31667'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yoshihiro-kawamura/furima-31667.git'
