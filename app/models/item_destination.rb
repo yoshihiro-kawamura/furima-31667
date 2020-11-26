65ede4fe1b75f7e7043d71389287bf50ede71a29
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-26 12:15:18 +0900

    モデルの日本語書き換え

--- app/models/item_destination.rb
+++ app/models/item_destination.rb
@@ -4,13 +4,15 @@ class ItemDestination
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :city, presence: { message: "can't be blank" }
-    validates :house_number, presence: { message: "can't be blank" }
-    validates :phone_number, format: { with: /\A\d{11}\z/, message: "can't be blank" }
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 0, message: 'を入力してください'}
+    validates :city
+    validates :house_number
+    validates :phone_number
     validates :token
   end
+  validates :postal_code, format:           { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'を入力してください' }, allow_blank: true
+  validates :phone_number, format:          { with: /\A\d{11}\z/, message: 'を入力してください'}, allow_blank: true
 
   def save
     # 配送先情報の登録
